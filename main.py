0ca33fb88009f8d5b065ece80a2287f39b8ea32a
Dino <dino.devedzic2018@gmail.com>
2026-05-07 18:53:45 +0200

    Pingvini po vrsti

--- main.py
+++ main.py
@@ -7,5 +7,15 @@ import scipy.stats as stats
 sns.set_theme(style="whitegrid")
 
 df = sns.load_dataset("penguins")
+
+print(df.head(8))
+print(df.info())
+
+
+print(df.isnull().sum())
+
+
+print(df["species"].value_counts())
+
+# Čišćenje
 df = df.dropna()
-print(f"Oblik skupa: {df.shape}")
